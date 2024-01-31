bd31c18d8a4a8ef6a33d1eb3f5a6b69e5f3ca332
Luca <Luca.Rettenberger@kit.edu>
2024-01-31 11:34:45 +0100

    update for 2024

--- checker.py
+++ checker.py
@@ -43,31 +43,30 @@ def test_normalize(x, x_norm):
         print(e)
 
 
-def test_forwardPass(forwardPass):
-    assert forwardPass(np.array([[0.2], [4]]), np.array([[0.1, 0.5]])) == 4.0804, "Given x and W  the output signal should be 4.0804"
-    print("Forward Pass was successful, you are ready to go.")
+def test_neural_network(marvin):
+    # TOODO
+    try:
+        assert 1==1
+        post_status(function_name='test_neural_network_success')
+        print("Neural network looks good, you are ready to go.")
+    except AssertionError as e:
+        post_status(function_name='test_neural_network_fail')
+        print(e)
 
 
-def test_objectiveFunction(objectiveFunction):
-    assert objectiveFunction(0, 1) == 1, "Given prediction 0 and label 1 the loss should be 1"
-    assert objectiveFunction(0, 0) == 0, "Given prediction 0 and label 0 the loss should be 0"
-    assert objectiveFunction(0.5, 1) == 0.25, "Given prediction 0.5 and label 1 the loss should be 0.25"
-    assert objectiveFunction(0.31415926, 1) == 0.47037752064374755, "Given prediction 0.31415926 and label 1 the loss should be 0.47037752064374755"
-    print("Your Objective Function was successful, you are ready to go.")
-
-def test_gradientFunction(gradientFunction):
-    assert gradientFunction( np.array([[2.0]]), np.array([[0.2], [4]]), 3, 2).all() == np.array([[1.6, 32.]]).all(), "You broke it, check the dimensions."
-    print("Gradient Function was successful, you are ready to go.")
-
-def test_update(update):
-    assert update(np.array([[ 1.6, 32. ]]), np.array([[ 1., 3. ]]), 1e-5).all() == np.array([[ 1.59999, 31.99997]]).all(), "Not quite right, the convention is to substract from the current weight."
-    print("Update function works fine, you are ready to go.")
-
+def test_accuracy(accuracy_func):
+    # TOODO
+    try:
+        assert 1==1
+        post_status(function_name='test_accuracy_success')
+        print("your metric looks good, you are ready to go.")
+    except AssertionError as e:
+        post_status(function_name='test_accuracy_fail')
+        print(e)
 
 ###############
 # LEADERBOARD #
 ###############
-
 
 def accuracy(y_pred,y):
     y_pred_argmax = torch.argmax(y_pred,dim=1)
